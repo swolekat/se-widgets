4a717d152f7ee5300b00a9e4969ce69c37911986
swolekat <swolecattwitch@gmail.com>
2022-06-12 23:48:58 -0500

    First iteration of TTS

---
+++ README.md
@@ -0,0 +1,5 @@
+# Stream Elements Widgets
+Collection of stream elements widgets made by yours truly
+
+## TTS
+Lets users the TTS. Has banned words. 
---
+++ tts/tts.js
@@ -0,0 +1,59 @@
+/*
+  "widgetUpdateUrl": {
+    "type": "hidden",
+    "value": "https://github.com/StreamElements/widgets/blob/master/WheelOfFortune/"
+  }
+ */
+
+
+let fieldData, apiToken;
+
+const sayMessage = (message) => {
+    const {voice, volume} = fieldData;
+    const url = `//api.streamelements.com/kappa/v2/speech?voice=${voice.replace('$', '')}&text=${encodeURI(message)}&key=${apiToken}`
+    const myAudio = new Audio(url);
+    myAudio.volume = volume;
+    myAudio.play();
+};
+
+const checkPrivileges = (data) => {
+    const {tags, userId} = data;
+    const {mod, subscriber, badges} = tags;
+    const required = fieldData.privileges;
+    const isMod = parseInt(mod);
+    const isSub = parseInt(subscriber);
+    const isVip = (badges.indexOf("vip") !== -1);
+    const isBroadcaster = (userId === tags['room-id']);
+    if (isBroadcaster) return true;
+    if (required === "mods" && isMod) return true;
+    if (required === "vips" && (isMod || isVip)) return true;
+    if (required === "subs" && (isMod || isVip || isSub)) return true;
+    return required === "everybody";
+};
+
+window.addEventListener('onEventReceived', function (obj) {
+    if (obj.detail.listener !== "message") {
+        return;
+    }
+    const {bannedWords, ttsCommand} = fieldData;
+    const data = obj.detail.event.data;
+    const {text} = data;
+    const textStartsWithCommand = text.toLowerCase().startsWith(ttsCommand.toLowerCase())
+    if (!textStartsWithCommand || !checkPrivileges(data)) {
+        return;
+    }
+
+
+    const bannedArray = bannedWords.split(',');
+    const messageHasBannedWords = bannedArray.some(word => text.toLowerCase().includes(word));
+    if(messageHasBannedWords){
+        return;
+    }
+    sayMessage(text.toLowerCase().replace(ttsCommand.toLowerCase(), '').trim());
+});
+
+window.addEventListener('onWidgetLoad', function (obj) {
+    fieldData = obj.detail.fieldData;
+    apiToken = obj.detail.channel.apiToken;
+});
+
