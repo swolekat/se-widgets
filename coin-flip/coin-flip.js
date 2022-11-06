c1905a28c650fa986c0801cc0677d343cd631cac
swolekat <swolecattwitch@gmail.com>
2022-11-06 15:04:45 -0600

    update images since they aren't the same size originally

--- coin-flip/coin-flip.js
+++ coin-flip/coin-flip.js
@@ -1,58 +1,19 @@
 let fieldData;
 
-let time = 0;
-let timeRemaining = 0;
-let interval;
-
 const mainContainer = document.getElementById('main-container');
-const timer = document.getElementById('timer');
-const sound = document.getElementById('sound');
-
-const formatTime = t => {
-    const minutes = Math.floor(t/60);
-    const seconds = t % 60;
-
-    return `${minutes}:${seconds >= 10 ? '' : '0'}${seconds}`;
-};
+const coin = document.getElementById('coin');
 
 const hide = () => mainContainer.className = 'main-container hidden';
 const show = () => mainContainer.className = 'main-container';
+let currentFace = 'heads';
+let isFlipping = false;
 
-const tick = () => {
-    timeRemaining = timeRemaining - 1;
-    timer.innerHTML = formatTime(timeRemaining);
-    show();
-    if(timeRemaining !== 0){
-        return;
+const getNextFace = () => {
+    const number = Math.random();
+    if(number < 0.5){
+        return 'heads';
     }
-    clearInterval(interval);
-    sound.play();
-    setTimeout(() => {
-        hide();
-    }, 4000);
-};
-
-const start = (duration) => {
-    time = duration * 60;
-    timeRemaining = time + 1;
-    if(interval){
-        clearInterval(interval);
-    }
-    interval = setInterval(tick, 1000);
-};
-
-const end = () => {
-    if(interval){
-        clearInterval(interval);
-    }
-    hide();
-};
-
-const restart = () => {
-    timeRemaining = time + 1;
-    if(!interval){
-        interval = setInterval(tick, 1000);
-    }
+    return 'tails';
 };
 
 
@@ -73,32 +34,31 @@ const checkPrivileges = (data) => {
 
 const handleMessage = (obj) => {
     const data = obj.detail.event.data;
+    const {text} = data;
 
     if (!checkPrivileges(data)) {
         return;
     }
 
-    const { startCommand, endCommand, restartCommand } = fieldData;
-    const {text} = data;
-    const startsWithStart = text.toLowerCase().startsWith(startCommand.toLowerCase());
-    const startsWithEnd = text.toLowerCase().startsWith(endCommand.toLowerCase());
-    const startsWithRestart = text.toLowerCase().startsWith(restartCommand.toLowerCase());
+    const { flipCommand, } = fieldData;
+    const startsWithFlip = text.toLowerCase().startsWith(flipCommand.toLowerCase());
 
-    if(startsWithStart){
-        const duration = text.toLowerCase().replace(startCommand.toLowerCase(), '').trim();
-        start(Number.parseInt(duration));
+    if(!startsWithFlip || isFlipping){
         return;
     }
 
-    if(startsWithEnd){
-        end();
-        return;
-    }
+    isFlipping = true;
 
-    if(startsWithRestart){
-        restart();
-        return;
-    }
+    show();
+    setTimeout(() => {
+        const nextFace = getNextFace();
+        coin.className = `coin ${currentFace}-to-${nextFace}`;
+        currentFace = nextFace;
+        setTimeout(() => {
+            hide();
+            isFlipping = false;
+        }, 5000);
+    }, 2000);
 };
 
 window.addEventListener('onEventReceived', function (obj) {
@@ -111,10 +71,8 @@ window.addEventListener('onEventReceived', function (obj) {
 window.addEventListener('onWidgetLoad', function (obj) {
     fieldData = obj.detail.fieldData;
 
-    const {volume, preview} = fieldData;
-    if(preview){
+    if(fieldData.preview){
         show();
     }
-    sound.volume = volume;
 });
 
