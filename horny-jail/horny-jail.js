4b7c4f3361fc3fef1a2dd0dfa797ae562b1423ed
swolekat <swolecattwitch@gmail.com>
2023-01-28 12:16:19 -0600

    horny-jail: finished 0.1

--- horny-jail/horny-jail.js
+++ horny-jail/horny-jail.js
@@ -4,35 +4,29 @@ const bonkQueue = [];
 let isBonking = false;
 
 const mainContainer = document.getElementById('main-container');
-const bonkee = document.getElementById('bonkee');
-const bat = document.getElementById('bat');
+const jailed = document.getElementById('jailed');
+const bars = document.getElementById('bars');
 const sound = document.getElementById('sound');
 
 const hideJail = () => {
     mainContainer.className = 'main-container hidden';
-
+    bars.className = 'bars';
 };
 
 const jailUser = (user) => {
+    hideJail();
     return new Promise((resolve) => {
         fetch(`https://decapi.me/twitch/avatar/${user}`)
             .then((data) => {
                 data.text().then((img) => {
-                    bonkee.src = img;
+                    jailed.src = img;
                     mainContainer.className = 'main-container';
                     setTimeout(() => {
-                        bat.className = 'bonk animate';
+                        bars.className = 'bars animate';
                         sound.play();
                         setTimeout(() => {
-                            bonkee.className = 'bonkee animate';
-                            setTimeout(() => {
-                                mainContainer.className = 'main-container hidden';
-                                bat.className = 'bonk';
-                                bonkee.className = 'bonkee';
-                                resolve();
-                            }, 1000);
-
-                        }, 500);
+                            hideJail();
+                        }, fieldData.howLongSeconds * 1000);
                     }, 1000);
                 }).catch(() => {
                     resolve();
@@ -48,13 +42,13 @@ const jailUser = (user) => {
 const handleMessage = (obj) => {
     const jailWords = (fieldData.jailWords || '').split(',');
     const data = obj.detail.event.data;
-    const {text, name} = data;
+    const {text, displayName} = data;
     const words = text.split(' ');
     const shouldGoToJail = words.some(word => jailWords.includes(word.toLowerCase()));
     if(!shouldGoToJail){
         return;
     }
-    jailUser(name);
+    jailUser(displayName);
 };
 
 window.addEventListener('onEventReceived', function (obj) {
