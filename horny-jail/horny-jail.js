e605489dc95b7de6ff8cd4c96a95a96e6261680c
swolekat <swolecattwitch@gmail.com>
2023-03-04 16:25:37 -0600

    horny jail: let people use it explicitly.

--- horny-jail/horny-jail.js
+++ horny-jail/horny-jail.js
@@ -5,6 +5,22 @@ const jailed = document.getElementById('jailed');
 const bars = document.getElementById('bars');
 const sound = document.getElementById('sound');
 
+const checkPrivileges = (data, privileges) => {
+    const {tags, userId} = data;
+    const {mod, subscriber, badges} = tags;
+    const required = privileges || fieldData.privileges;
+    const isMod = parseInt(mod);
+    const isSub = parseInt(subscriber);
+    const isVip = (badges.indexOf("vip") !== -1);
+    const isBroadcaster = (userId === tags['room-id']);
+    if (isBroadcaster) return true;
+    if (required === "justSubs" && isSub) return true;
+    if (required === "mods" && isMod) return true;
+    if (required === "vips" && (isMod || isVip)) return true;
+    if (required === "subs" && (isMod || isVip || isSub)) return true;
+    return required === "everybody";
+};
+
 const hideJail = () => {
     mainContainer.className = 'main-container hidden';
     bars.className = 'bars';
@@ -43,11 +59,24 @@ let cooldownTimeout;
 
 const handleMessage = (obj) => {
     const jailWords = (fieldData.jailWords || '').split(',');
+    const {jailCommand, privileges} = fieldData;
     const data = obj.detail.event.data;
     const {text, displayName} = data;
+
+    if(cooldownTimeout){
+        return;
+    }
+
+    if(text.startsWith(jailCommand) && checkPrivileges(data, privileges)){
+        const userToJail = text.replace(jailCommand, '').replace('@', '').trim();
+        jailUser(userToJail);
+        return;
+    }
+
     const words = text.replace(/[.,\/#!$%\^&\*;:{}=\-_`~()]/g,'').split(' ');
     const shouldGoToJail = words.some(word => jailWords.includes(word.toLowerCase()));
-    if(!shouldGoToJail || cooldownTimeout){
+    if(!shouldGoToJail){
+
         return;
     }
     jailUser(displayName);
