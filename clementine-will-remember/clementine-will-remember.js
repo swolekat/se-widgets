7859f63f716f4eb6183aa05d834dc450b6a1d705
swolekat <swolecattwitch@gmail.com>
2023-10-06 01:37:06 -0500

    clementine assets

---
+++ clementine-will-remember/clementine-will-remember.js
@@ -0,0 +1,66 @@
+let fieldData;
+
+const mainContainer = document.getElementById('main-container');
+const text = document.getElementById('text');
+
+let isShowing = false;
+
+const hide = () => mainContainer.className = 'main-container hidden';
+const show = () => mainContainer.className = 'main-container';
+
+const checkPrivileges = (data, privileges) => {
+    const {tags, userId} = data;
+    const {mod, subscriber, badges} = tags;
+    const required = privileges || fieldData.privileges;
+    const isMod = parseInt(mod);
+    const isSub = parseInt(subscriber);
+    const isVip = (badges.indexOf("vip") !== -1);
+    const isBroadcaster = (userId === tags['room-id']);
+    if (isBroadcaster) return true;
+    if (required === "justSubs" && isSub) return true;
+    if (required === "mods" && isMod) return true;
+    if (required === "vips" && (isMod || isVip)) return true;
+    if (required === "subs" && (isMod || isVip || isSub)) return true;
+    return required === "everybody";
+};
+
+const handleMessage = (obj) => {
+    const data = obj.detail.event.data;
+    const {text} = data;
+
+    if (!checkPrivileges(data)) {
+        return;
+    }
+
+    const command = fieldData.command;
+    const startsWithCommand = text.toLowerCase().startsWith(command.toLowerCase());
+
+    if(!startsWithCommand || isShowing){
+        return;
+    }
+
+    isShowing = true;
+    const whoWillRemember = text.toLowerCase().replace(command.toLowerCase(), '').trim();
+    text.innerText = whoWillRemember;
+
+    show();
+    setTimeout(() => {
+        hide();
+    }, 5000);
+};
+
+window.addEventListener('onEventReceived', function (obj) {
+    if (obj.detail.listener !== "message") {
+        return;
+    }
+    handleMessage(obj);
+});
+
+window.addEventListener('onWidgetLoad', function (obj) {
+    fieldData = obj.detail.fieldData;
+
+    if(fieldData.preview){
+        show();
+    }
+});
+
