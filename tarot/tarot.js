1ea88af85dbf00e4359274fb523abaa3a5eabcd0
swolekat <swolecattwitch@gmail.com>
2023-11-25 01:24:52 -0600

    tarot widget

--- tarot/tarot.js
+++ tarot/tarot.js
@@ -1,57 +1,97 @@
 let fieldData;
 
-const bonkQueue = [];
-let isBonking = false;
+const listOfCards = [
+    "aceOfCups",
+    "aceOfPentacles",
+    "aceOfSwords",
+    "aceOfWands",
+    "chariot",
+    "death",
+    "devil",
+    "eightOfCups",
+    "eightOfPentacles",
+    "eightOfSwords",
+    "eightOfWands",
+    "emperor",
+    "empress",
+    "fiveOfCups",
+    "fiveOfPentacles",
+    "fiveOfSwords",
+    "fiveOfWands",
+    "fool",
+    "fourOfCups",
+    "fourOfPentacles",
+    "fourOfSwords",
+    "fourOfWands",
+    "hangedMan",
+    "hermit",
+    "hierophant",
+    "highPriestess",
+    "judgement",
+    "justice",
+    "kingOfCups",
+    "kingOfPentacles",
+    "kingOfSwords",
+    "kingOfWands",
+    "knightOfCups",
+    "knightOfPentacles",
+    "knightOfSwords",
+    "knightOfWands",
+    "lovers",
+    "magician",
+    "moon",
+    "nineOfCups",
+    "nineOfPentacles",
+    "nineOfSwords",
+    "nineOfWands",
+    "pageOfCups",
+    "pageOfPentacles",
+    "pageOfSwords",
+    "pageOfWands",
+    "queenOfCups",
+    "queenOfPentacles",
+    "queenOfSwords",
+    "queenOfWands",
+    "sevenOfCups",
+    "sevenOfPentacles",
+    "sevenOfSwords",
+    "sevenOfWands",
+    "sixOfCups",
+    "sixOfPentacles",
+    "sixOfSwords",
+    "sixOfWands",
+    "star",
+    "strength",
+    "sun",
+    "temperance",
+    "tenOfCups",
+    "tenOfPentacles",
+    "tenOfSwords",
+    "tenOfWands",
+    "threeOfCups",
+    "threeOfPentacles",
+    "threeOfSwords",
+    "threeOfWands",
+    "tower",
+    "twoOfCups",
+    "twoOfPentacles",
+    "twoOfSwords",
+    "twoOfWands",
+    "wheelOfFortune",
+    "world",
+];
+let currentCardIndex = 0;
 
 const mainContainer = document.getElementById('main-container');
-const bonkee = document.getElementById('bonkee');
-const bat = document.getElementById('bat');
-const sound = document.getElementById('sound');
+const card1 = document.getElementById('card1');
+const card2 = document.getElementById('card2');
+const card3 = document.getElementById('card3');
+const cardElements = [card1, card2, card3];
+const cardWrapper1 = document.getElementById('cardWrapper1');
+const cardWrapper3 = document.getElementById('cardWrapper3');
+const cardWrapper2 = document.getElementById('cardWrapper2');
+const cardWrappers = [cardWrapper1, cardWrapper3, cardWrapper2];
 
-const bonkUser = (user) => {
-    return new Promise((resolve) => {
-        fetch(`https://decapi.me/twitch/avatar/${user}`)
-            .then((data) => {
-                data.text().then((img) => {
-                    bonkee.src = img;
-                    mainContainer.className = 'main-container';
-                    setTimeout(() => {
-                        bat.className = 'bonk animate';
-                        sound.play();
-                        setTimeout(() => {
-                            bonkee.className = 'bonkee animate';
-                            setTimeout(() => {
-                                mainContainer.className = 'main-container hidden';
-                                bat.className = 'bonk';
-                                bonkee.className = 'bonkee';
-                                resolve();
-                            }, 1000);
-
-                        }, 500);
-                    }, 1000);
-                }).catch(() => {
-                    resolve();
-                });
-            })
-            .catch(() => {
-                resolve();
-            });
-    });
-
-};
-
-const tarot = async () => {
-    if(isBonking){
-        return;
-    }
-    isBonking = true;
-
-    while(bonkQueue.length > 0){
-        const queueUser = bonkQueue.shift();
-        await bonkUser(queueUser);
-    }
-    isBonking = false;
-};
 
 const checkPrivileges = (data, privileges) => {
     const {tags, userId} = data;
@@ -69,18 +109,71 @@ const checkPrivileges = (data, privileges) => {
     return required === "everybody";
 };
 
+const getIsReversed = () => {
+    const rando = Math.round(Math.random() * 1000) %2;
+    return rando === 1 ? 'reversed' : '';
+};
+
+const drawThree = () => {
+    const numberOfCards = listOfCards.length;
+    const cardIndexes = [];
+    while(cardIndexes.length <3) {
+        const randomIndex = Math.round(Math.random() * 1000) % numberOfCards;
+        if(!cardIndexes.includes(randomIndex)){
+            cardIndexes.push(randomIndex);
+        }
+    }
+    cardIndexes.forEach((cardIndex, index) => {
+        const element = cardElements[index];
+        element.src = `https://swolekat.github.io/se-widgets/assets/tarot/${listOfCards[cardIndex]}.jpg`;
+        element.className = `card ${getIsReversed()}`;
+        const wrapperElement = cardWrappers[index];
+        wrapperElement.className = 'card-wrapper';
+    });
+};
+
+const showCard = (cardToShow) => {
+    const cardParts = cardToShow.split(' ');
+    const element = cardElements[currentCardIndex];
+    element.src = `https://swolekat.github.io/se-widgets/assets/tarot/${cardParts[0]}.jpg`;
+    element.className = `card ${cardParts.length > 1 ? 'reversed' : ''}`
+    const wrapperElement = cardWrappers[currentCardIndex];
+    wrapperElement.className = 'card-wrapper';
+    currentCardIndex = (currentCardIndex + 1) % cardElements.length;
+};
+
+const hide = () => {
+    currentCardIndex = 0;
+    cardWrapper1.className = 'card-wrapper hidden';
+    cardWrapper2.className = 'card-wrapper hidden';
+    cardWrapper3.className = 'card-wrapper hidden';
+};
+
 const handleMessage = (obj) => {
-    const bonkCommand = fieldData.bonkCommand;
+    const {drawThreeCommand, showCommand, hideCommand} = fieldData;
     const data = obj.detail.event.data;
-    const {text} = data;
-    const textStartsWithCommand = text.toLowerCase().startsWith(bonkCommand.toLowerCase());
-    if (!textStartsWithCommand || !checkPrivileges(data)) {
+    if (!checkPrivileges(data)) {
         return;
     }
 
-    const userToBonk = text.toLowerCase().replace(bonkCommand.toLowerCase(), '').trim();
-    bonkQueue.push(userToBonk);
-    tarot();
+    const {text} = data;
+    const isDrawThree = text.toLowerCase().startsWith(drawThreeCommand.toLowerCase());
+    if (isDrawThree) {
+        drawThree();
+        return;
+    }
+
+    const show = text.toLowerCase().startsWith(showCommand.toLowerCase());
+    if (show) {
+        showCard(text.replace(showCommand, '').trim());
+        return;
+    }
+
+    const shouldHide = text.toLowerCase().startsWith(hideCommand.toLowerCase());
+    if (shouldHide) {
+        hide();
+        return;
+    }
 };
 
 window.addEventListener('onEventReceived', function (obj) {
@@ -92,5 +185,11 @@ window.addEventListener('onEventReceived', function (obj) {
 
 window.addEventListener('onWidgetLoad', function (obj) {
     fieldData = obj.detail.fieldData;
+    if (fieldData.preview) {
+        // show all cards here
+        cardWrappers.className = 'card-wrapper';
+        cardWrappers.className = 'card-wrapper';
+        cardWrappers.className = 'card-wrapper';
+    }
 });
 
