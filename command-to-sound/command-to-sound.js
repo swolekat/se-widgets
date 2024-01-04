97a61f8d12cb0b22177a5016e630289614c9602b
swolekat <swolecattwitch@gmail.com>
2024-01-04 16:52:09 -0600

    new widget: command to sound. Enter a command and play a sound

---
+++ command-to-sound/command-to-sound.js
@@ -0,0 +1,55 @@
+let fieldData;
+
+const mainContainer = document.getElementById('main-container');
+const sound = document.getElementById('sound');
+
+const checkPrivileges = (data, privileges) => {
+    const {tags, userId} = data;
+    const {mod, subscriber, badges} = tags;
+    const required = privileges || fieldData.privileges;
+    const isMod = parseInt(mod);
+    const isSub = parseInt(subscriber);
+    const isVip = (badges.indexOf("vip") !== -1);
+    const isBroadcaster = (userId === tags['room-id']);
+    if (isBroadcaster) return true;
+    if (required === "justSubs" && isSub) return true;
+    if (required === "mods" && isMod) return true;
+    if (required === "vips" && (isMod || isVip)) return true;
+    if (required === "subs" && (isMod || isVip || isSub)) return true;
+    return required === "everybody";
+};
+
+let cooldownTimeout;
+
+const playSound = () => {
+    sound.play();
+    cooldownTimeout = setTimeout(() => {
+        cooldownTimeout = undefined;
+    }, fieldData.cooldown * 1000);
+};
+
+const handleMessage = (obj) => {
+    const command = fieldData.command;
+    const data = obj.detail.event.data;
+    const {text} = data;
+    const textStartsWithCommand = text.startsWith(command);
+    if(!textStartsWithCommand || !checkPrivileges(data)){
+        return;
+    }
+    if(cooldownTimeout){
+        return;
+    }
+    playSound();
+};
+
+window.addEventListener('onEventReceived', function (obj) {
+    if (obj.detail.listener !== "message") {
+        return;
+    }
+    handleMessage(obj);
+});
+
+window.addEventListener('onWidgetLoad', function (obj) {
+    fieldData = obj.detail.fieldData;
+});
+
